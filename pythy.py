ee4877b2c87d3eda4ceb2fe1d30085f1a1dba5da
Wael Elsebaiy <wsebaiy@googlemail.com>
2026-08-27 17:10:49 +0200

    added update function

--- pythy.py
+++ pythy.py
@@ -4,8 +4,9 @@ def show_menu():
     print("===== Patient Manager =====")
     print("1. Add Patient")
     print("2. Show all patients")
-    print("3. Delete Patient")
-    print("4. Exit")
+    print("3. Update patients")
+    print("4. Delete Patient")
+    print("5. Exit")
     print('')
 
 def add_patient(patients):       
@@ -44,6 +45,39 @@ def delete_patient(patients):
         print(f"{patients[del_patient_nu-1]['name']} deleted successfully!")
         del(patients[del_patient_nu-1])
 
+def update_patient(patients):
+    if not patients:
+        print("No patients found!")
+        return
+    for index,patient in enumerate(patients,1):
+        print(f"{index}. {patient['name']} ({patient['age']} years)")
+        
+    try:
+        update_patient_nu = int(input("Enter patient number to update: ")) 
+    except ValueError:
+        print("Invalid Entry.")
+        return
+    if update_patient_nu < 1 or update_patient_nu > len(patients):
+        print("Invalid Number.") 
+        return
+    patient_index = update_patient_nu -1
+    new_name = input("New name: ")
+    while True:
+        try:
+            new_age = int(input("New age: "))
+        except ValueError:
+            print("Invalid Entry.")
+        else:
+            break    
+            
+    patients[patient_index]["name"] = new_name
+    patients[patient_index]["age"] = new_age
+    print("\n")
+    print("Patient Updated Successfully!")
+    return
+
+
+
 patients =[]
 while True:
     show_menu()
@@ -53,11 +87,14 @@ while True:
         
     elif choice == '2':
         show_patients(patients)
-        
+
     elif choice =='3':
+            update_patient(patients)
+        
+    elif choice =='4':
         delete_patient(patients)    
 
-    elif choice == '4':
+    elif choice == '5':
         print("Goodbye!")
         break
     else:
